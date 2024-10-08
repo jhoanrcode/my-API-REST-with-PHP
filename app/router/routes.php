6996e693d576641d24a132f51ce1c1d9981becff
Jhoan Avila <108818345+jhoanrcode@users.noreply.github.com>
2024-10-08 17:41:30 -0500

    Update routes.php

--- app/router/routes.php
+++ app/router/routes.php
@@ -10,9 +10,4 @@ $routes = [
   '/users/update'      => 'UserService@update',
   '/users/remove/{id}' => 'UserService@remove',
   
-  '/books'             => 'BookService@index',
-  '/books/create'      => 'BookService@create',
-  '/books/list/{id}'   => 'BookService@listById',
-  '/books/update/{id}' => 'BookService@update',
-  '/books/remove/{id}' => 'BookService@remove',
 ];
